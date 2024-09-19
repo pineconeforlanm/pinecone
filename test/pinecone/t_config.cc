5e18d43f59d8aceb404aad68b8c7700337273105
pineconeforlanm <pineconeforlanm@outlook.com>
2024-09-19 17:35:48 +0800

    test: fix function

--- test/pinecone/t_config.cc
+++ test/pinecone/t_config.cc
@@ -16,8 +16,7 @@ class Pinecone {
 
   Pinecone(std::string_view name, int age) : name_(name), age_(age){};
 
-  friend auto operator<<(std::ostream& os,
-                         const Pinecone& pinecone) -> std::ostream& {
+  friend auto operator<<(std::ostream& os, const Pinecone& pinecone) -> std::ostream& {
     os << pinecone.name_ << " " << pinecone.age_;
     return os;
   }
@@ -28,37 +27,31 @@ class Pinecone {
   int age_{};
 };
 
-auto g_person_val_config = pinecone::config::Config::lookup(
-    "root.ref.family.person", ConfigPerson{"bob", 36},
-    "system ref family person");
+auto g_person_val_config =
+    pinecone::config::Config::Lookup("root.ref.family.person", ConfigPerson{"bob", 36}, "system ref family person");
 
-auto g_pinecone_val_config = pinecone::config::Config::lookup(
-    "root.ref.family.pinecone", Pinecone{"body", 366},
-    "system ref family pinecone");
+auto g_pinecone_val_config =
+    pinecone::config::Config::Lookup("root.ref.family.pinecone", Pinecone{"body", 366}, "system ref family pinecone");
 
-auto g_no_int_config_var =
-    pinecone::config::Config::lookup("system.no_int", 8080, "system no_int");
+auto g_no_int_config_var = pinecone::config::Config::Lookup("system.no_int", 8080, "system no_int");
 
-auto g_no_double_config_var =
-    pinecone::config::Config::lookup("double", 6.3699, "double");
+auto g_no_double_config_var = pinecone::config::Config::Lookup("double", 6.3699, "double");
 
-auto test_02() -> void {
-  ELOGI << g_no_int_config_var->get_value();
-  ELOGI << g_no_double_config_var->get_value();
-  ELOGI << g_pinecone_val_config->get_value();
-  ELOGI << g_person_val_config->get_value().name << ":"
-        << g_person_val_config->get_value().age;
-  pinecone::config::Config::load_from_conf_dir();
-  ELOGI << g_no_int_config_var->get_value();
-  ELOGI << g_no_double_config_var->get_value();
-  ELOGI << g_pinecone_val_config->get_value();
-  ELOGI << g_person_val_config->get_value().name << ":"
-        << g_person_val_config->get_value().age;
+auto Test02() -> void {
+  ELOGI << g_no_int_config_var->GetValue();
+  ELOGI << g_no_double_config_var->GetValue();
+  ELOGI << g_pinecone_val_config->GetValue();
+  ELOGI << g_person_val_config->GetValue().name << ":" << g_person_val_config->GetValue().age;
+  pinecone::config::Config::LoadFromConfDir();
+  ELOGI << g_no_int_config_var->GetValue();
+  ELOGI << g_no_double_config_var->GetValue();
+  ELOGI << g_pinecone_val_config->GetValue();
+  ELOGI << g_person_val_config->GetValue().name << ":" << g_person_val_config->GetValue().age;
 }
 
 auto main() -> int {
   easylog::set_async(false);
   easylog::set_console(true);
-  test_02();
+  Test02();
   return 0;
 }
